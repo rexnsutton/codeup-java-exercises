960e20de20cc2a0d3c04afedd8042429e2e152d6
Student Name <rexsutton2@gmail.cogit>
2020-05-01 09:49:23 -0500

    finished through #13

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,22 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int myFavoriteNumber = 5;
+        System.out.println(myFavoriteNumber);
+        String myString = "turtles?";
+        System.out.println(myString);
+//        Float myNumber = ;
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
     }
 }
