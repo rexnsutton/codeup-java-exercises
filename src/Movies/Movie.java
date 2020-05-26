6e4fb9a6866f30a3c8a15a9bf3e8514068f77433
Student Name <rexsutton2@gmail.cogit>
2020-05-25 21:44:00 -0500

    movies array added

---
+++ src/Movies/Movie.java
@@ -0,0 +1,12 @@
+package Movies;
+
+public class Movie {
+    private String name;
+    private String category;
+
+    public Movie(String name, String category) {
+        this.name = name;
+        this.category = category;
+
+    }
+}
--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,8 +1,18 @@
+import java.util.Arrays;
+
 public class ServerNameGenerator {
     String[] adjs = {"spicy", "sweet", "tasty", "sharp", "bumpy", "sour", "freezing", "hard", "cool", "slimy", "icy"};
     String[] nouns = {"rex", "john", "edith", "ry", "willie", "janice", "casey", "tracy", "lily", "mary"};
     public static void main(String[] args) {
-
+        System.out.println("Here is your server name: ");
     }
+    public String[] randomAdj(String[] adjs){
+
+        for (int i = 0; i <= adjs.length; i++){
+            i += adjs.length;
+            System.out.println(i);
+        }
+    }
+
 
 }
